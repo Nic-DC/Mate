a7a0e846d31481fac07f495aba47f570459e74cf
Nic <nic.dan.costea@gmail.com>
2023-02-28 12:34:53 +0100

    the "typing" event

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -6,6 +6,7 @@ import Card from "@mui/material/Card";
 import { Typography } from "@mui/material";
 
 import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
 
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
@@ -15,6 +16,7 @@ const ChatWindow = () => {
   const [socket, setSocket] = useState(null); // socket
   const [message, setMessage] = useState(""); // input message
   const [chat, setChat] = useState([]); // all inputted messages
+  const [isTyping, setIsTyping] = useState(false);
 
   console.log("The chat: ", chat);
 
@@ -27,19 +29,43 @@ const ChatWindow = () => {
   useEffect(() => {
     if (!socket) return;
 
-    socket.on("server-message", (message) => {
-      console.log("FE - the server-message: ", message);
+    socket.on("typing-started-server", () => {
+      console.log("typing...");
+      setIsTyping(true);
+    });
+    socket.on("typing-stopped-server", () => {
+      console.log("stoppeedddd...");
+      setIsTyping(false);
+    });
+
+    socket.on("message-server", (message) => {
+      console.log("FE - the message-server: ", message);
+      // we save the message and received property that we receive from the server
       setChat((prev) => [...prev, { message: message.message, received: true }]);
     });
   }, [socket]);
+
+  // handleInput function
+  const handleInput = (e) => {
+    setMessage(e.target.value);
+    socket.emit("typing-started-client");
+
+    setTimeout(() => {
+      socket.emit("typing-stopped-client");
+    }, 600);
+  };
 
   // when we send the message
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    /* --- sending the message ---*/
     // we emit the form input
-    socket.emit("client-message", { message });
+    socket.emit("message-client", { message });
+    // save the message & received property that we emit
     setChat((prev) => [...prev, { message, received: false }]);
+
+    /* --- emit to the server that we are typing a message ---*/
 
     // we clear the form input
     setMessage("");
@@ -48,19 +74,28 @@ const ChatWindow = () => {
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <Card sx={{ padding: 2, marginTop: 10, width: "60%", backgroundColor: "lightgray" }}>
         <Box sx={{ marginBottom: 5 }}>
-          {chat.map((data) => (
-            <Typography sx={{ textAlign: data.received ? "right" : "left" }}>{data.message}</Typography>
+          {isTyping && (
+            <InputLabel shrink htmlFor="message-input">
+              Typing...
+            </InputLabel>
+          )}
+
+          {chat.map((data, index) => (
+            <Typography key={index} sx={{ textAlign: data.received ? "left" : "right" }}>
+              {data.message}
+            </Typography>
           ))}
         </Box>
 
         <Box component="form" onSubmit={handleSubmit}>
           <OutlinedInput
+            id="message-input"
             sx={{ backgroundColor: "white" }}
             fullWidth
             placeholder="write here"
             size="small"
             value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={handleInput}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton type="submit" edge="end">
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,11 +1,21 @@
 export const handleSocket = (socket) => {
   console.log("NEW CONNECTION WITH SOCKET ID: ", socket.id);
 
-  socket.on("client-message", (message) => {
+  socket.on("typing-started-client", () => {
+    console.log("tyyyyyyweee");
+    socket.broadcast.emit("typing-started-server");
+  });
+
+  socket.on("typing-stopped-client", () => {
+    console.log("stoppppp");
+    socket.broadcast.emit("typing-stopped-server");
+  });
+
+  socket.on("message-client", (message) => {
     // const sentMessage = message.message;
     console.log(`Message from ${socket.id} - received: `, message);
 
-    socket.broadcast.emit("server-message", { message: message.message });
+    socket.broadcast.emit("message-server", { message: message.message });
     // const editedMessage = `${message.message} => BE`;
     // socket.broadcast.emit("server-message", { editedMessage });
   });
